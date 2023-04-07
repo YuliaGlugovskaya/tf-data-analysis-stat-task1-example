150388301c194e6f38276b03a3e233bc8b6eb388
YuliaGlugovskaya <124707602+YuliaGlugovskaya@users.noreply.github.com>
2023-04-07 17:37:32 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,5 +5,5 @@ from scipy.stats import chi2
 chat_id = 206038589 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    a = 2*(x.mean() - 1)/44*44
+    a = 2*(x.mean() - 1)/(44*44)
     return a # Ваш ответ
